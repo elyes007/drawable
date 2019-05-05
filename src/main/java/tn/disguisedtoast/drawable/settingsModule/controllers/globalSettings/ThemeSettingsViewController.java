a68259fd2e5855054cd240bdbb6c45c82109d960
Seif Abdennadher <sarahelheni@gmail.com>
2019-05-05 17:03:58 +0100

    Finished theme colors settings

--- src/main/java/tn/disguisedtoast/drawable/settingsModule/controllers/globalSettings/ThemeSettingsViewController.java
+++ src/main/java/tn/disguisedtoast/drawable/settingsModule/controllers/globalSettings/ThemeSettingsViewController.java
@@ -2,22 +2,28 @@ package tn.disguisedtoast.drawable.settingsModule.controllers.globalSettings;
 
 import com.helger.css.ECSSVersion;
 import com.helger.css.decl.CSSDeclarationList;
+import com.helger.css.decl.CSSExpression;
 import com.helger.css.reader.CSSReaderDeclarationList;
 import com.helger.css.writer.CSSWriter;
 import com.helger.css.writer.CSSWriterSettings;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 import tn.disguisedtoast.drawable.ProjectMain.Drawable;
+import tn.disguisedtoast.drawable.settingsModule.interfaces.SettingsControllerInterface;
+import tn.disguisedtoast.drawable.settingsModule.utils.CustomColorPicker;
+import tn.disguisedtoast.drawable.settingsModule.utils.FxUtils;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ResourceBundle;
 
-public class ThemeSettingsViewController implements Initializable {
+public class ThemeSettingsViewController implements Initializable, SettingsControllerInterface {
 
     @FXML
     private Label baseColorPicker;
@@ -30,21 +36,57 @@ public class ThemeSettingsViewController implements Initializable {
 
     private String themeName;
 
+    private CSSDeclarationList cssDeclarations;
+
+    private CSSWriter aWriter = new CSSWriter(new CSSWriterSettings(ECSSVersion.CSS30, false));
+    private Path themesPath;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        System.out.println("Theme Settings");
+
+        this.baseColorPicker.setGraphic(new CustomColorPicker());
+        this.contrastColorPicker.setGraphic(new CustomColorPicker());
+        this.shadeColorPicker.setGraphic(new CustomColorPicker());
+        this.tintColorPicker.setGraphic(new CustomColorPicker());
+
+        themesPath = Paths.get((Drawable.projectPath + "&RelatedFiles&themes").replace("&", File.separator));
+
+        ((CustomColorPicker) this.baseColorPicker.getGraphic()).setOnAction(event -> {
+            this.cssDeclarations.getDeclarationOfPropertyName("--ion-color-" + themeName).setExpression(CSSExpression.createSimple(FxUtils.toRGBCode(((CustomColorPicker) event.getSource()).getValue())));
+        });
+        ((CustomColorPicker) this.contrastColorPicker.getGraphic()).setOnAction(event -> {
+            this.cssDeclarations.getDeclarationOfPropertyName("--ion-color-" + themeName + "-contrast").setExpression(CSSExpression.createSimple(FxUtils.toRGBCode(((CustomColorPicker) event.getSource()).getValue())));
+        });
+        ((CustomColorPicker) this.shadeColorPicker.getGraphic()).setOnAction(event -> {
+            this.cssDeclarations.getDeclarationOfPropertyName("--ion-color-" + themeName + "-shade").setExpression(CSSExpression.createSimple(FxUtils.toRGBCode(((CustomColorPicker) event.getSource()).getValue())));
+        });
+        ((CustomColorPicker) this.tintColorPicker.getGraphic()).setOnAction(event -> {
+            this.cssDeclarations.getDeclarationOfPropertyName("--ion-color-" + themeName + "-tint").setExpression(CSSExpression.createSimple(FxUtils.toRGBCode(((CustomColorPicker) event.getSource()).getValue())));
+        });
     }
 
     public void setTheme(String themeName) {
         this.themeName = themeName;
 
         try {
-            String css = new String(Files.readAllBytes(Paths.get((Drawable.projectPath + "&RelatedFiles&themes").replace("&", File.separator)))).replace(":root {", "").replace("}", "");
-            System.out.println(css);
-            CSSDeclarationList cssDeclarations = CSSReaderDeclarationList.readFromString(css, ECSSVersion.CSS30);
+            String css = new String(Files.readAllBytes(themesPath)).replace(":root {", "").replace("}", "");
 
-            CSSWriter aWriter = new CSSWriter(new CSSWriterSettings(ECSSVersion.CSS30, false));
-            System.out.println(":root {\n" + aWriter.getCSSAsString(cssDeclarations) + "\n}");
+            cssDeclarations = CSSReaderDeclarationList.readFromString(css, ECSSVersion.CSS30);
+
+            ((CustomColorPicker) this.baseColorPicker.getGraphic()).setValue(Color.valueOf(cssDeclarations.getDeclarationOfPropertyName("--ion-color-" + themeName).getExpressionAsCSSString()));
+            ((CustomColorPicker) this.contrastColorPicker.getGraphic()).setValue(Color.valueOf(cssDeclarations.getDeclarationOfPropertyName("--ion-color-" + themeName + "-contrast").getExpressionAsCSSString()));
+            ((CustomColorPicker) this.shadeColorPicker.getGraphic()).setValue(Color.valueOf(cssDeclarations.getDeclarationOfPropertyName("--ion-color-" + themeName + "-shade").getExpressionAsCSSString()));
+            ((CustomColorPicker) this.tintColorPicker.getGraphic()).setValue(Color.valueOf(cssDeclarations.getDeclarationOfPropertyName("--ion-color-" + themeName + "-tint").getExpressionAsCSSString()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void save() {
+        try {
+            System.out.println("Saved");
+            Files.write(themesPath, aWriter.getCSSAsString(cssDeclarations).getBytes());
         } catch (IOException e) {
             e.printStackTrace();
         }
